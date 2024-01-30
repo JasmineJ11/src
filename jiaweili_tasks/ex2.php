f52dad91c9297a6574c30d70af3efb2d8f52fa24
Jiawei Li <jiawei23000@student.hamk.fi>
2024-01-30 15:25:44 +0200

    integrate header&footer to ex1/2.php
    header&footer  from final assignment

--- jiaweili_tasks/ex2.php
+++ jiaweili_tasks/ex2.php
@@ -3,11 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ex2</title>
+    <title>Ex2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/3743480f3c.js" crossorigin="anonymous"></script>
+
+    <style>
+        .content{
+            height: 70vh;
+        }
+    </style>
+
 </head>
 <body>
+<div class="container">
     <?php include 'header.php'; ?>
-   <h3> EX2:header&footer </h3>
+
+    <div class="content d-flex  ">
+        <h3> EX2:header&footer </h3>
+    </div>
+
    <?php include 'footer.php'; ?>
+</div>
 </body>
 </html>
--- jiaweili_tasks/header.php
+++ jiaweili_tasks/header.php
@@ -5,32 +5,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>header</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="https://kit.fontawesome.com/3743480f3c.js" crossorigin="anonymous"></script>
         <style>
-            nav{
-             font-family: Arial, sans-serif;
-             color:white;
-             font-size:30px;
+            body{
+                background-image: url("http://staff.hamk.fi/~tsaksa/temp/background-image.jpg");
             }
 
-            a{
-            color:gray;
+
+
+            main{
+                min-height: 600px;
             }
         </style>
 </head>
 <body>
-<div class="container-fluid d-flex justify-content-center" style="color: #003e40;background-color: #E8CCB0;">
-            <nav class="d-flex justify-content-around align-items-center" style="max-width: 1600px">
-                    <nav class="nav d-flex justify-content-center">
-                    <div class="p-2"><a href="ex1.php" class="link">EX1</a></div>
-                    <div class="p-2"><a href="ex2.php" class="link">EX2</a></div>
-                    <div class="p-2"><a href="ex3.php" class="link">EX3</a></div>
-                    <div class="p-2"><a href="ex4.php" class="link">EX4</a></div>
-                </nav>
-            </nav>
-</div>
+<div class="container">
+<header class="row">
+    <div class="col-sm-7 fs-2">
+        Jiawei Li
+    </div>
 
+    <nav class="col-sm-5 navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="ex1.php">EX1</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="ex2.php">EX2</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">EX3</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            EX4
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">111</a></li>
+                            <li><a class="dropdown-item" href="#">222</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    </div>
+</header>
 
 </body>
 </html>
