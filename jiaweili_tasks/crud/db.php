0f49dcc0c85bfecd8717f445ca639ccfc49c24c1
Jiawei Li <jiawei23000@student.hamk.fi>
2024-02-05 15:58:05 +0200

    crud

---
+++ jiaweili_tasks/crud/db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "web-dev-env-main-db-1";
+$username = "app1";
+$password = "password";
+$dbname = "app1";
+
+//create database connection
+$conn = new myspli ($servername,$username,$password,$dbname);
+
+//check connection
+if ($conn -> connect_error){
+    die("Connection Failed:" . $conn->connect_error);
+}
+
+?>
